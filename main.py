3e77d2a768ae50f62ea497f911a73071e53816ce
Theon87 <theonfrancis17@gmail.com>
2025-03-10 00:07:51 -0400

    adds main function for the game

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 # Rock-Paper-Scissors
+import random
 
-# get user choice
-# randomly generate computer choice
-# compare user choice to computer choice
-# determine winner
-# prompt user to play again
-# if user quits, show tally of wins and loses
+
+def play_game():
+    choices = ["r", "p", "s"]
+    # get user choice
+
+    is_playing = True
+
+    while is_playing:
+        user_choice = input(
+            "Enter rock(r), paper(p), or scissors(s): ").lower()
+        # randomly generate computer choice
+        computer_choice = random.choice(choices)
+        # compare user choice to computer choice
+        # determine winner
+        if user_choice == computer_choice:
+            print("Tie")
+        elif user_choice == "r" and computer_choice == "p":
+            print("You lose.")
+        elif user_choice == "p" and computer_choice == "s":
+            print("You lose.")
+        elif user_choice == "s" and computer_choice == "r":
+            print("You lose.")
+        else:
+            print("You win!")
+        # prompt user to play again
+        play_again = input("Play again? yes(y) or no(n): ")
+
+        if play_again == "y":
+            is_playing = True
+        else:
+            is_playing = False
+        # if user quits, show tally of wins and loses
+
+
+if __name__ == "__main__":
+    play_game()
