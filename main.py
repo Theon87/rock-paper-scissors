ce965dd398faf934e806ab99b56c41461bfea4ba
Theon87 <theonfrancis17@gmail.com>
2025-03-09 23:52:33 -0400

    pseudocode for rps game

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # Rock-Paper-Scissors
+
+# get user choice
+# randomly generate computer choice
+# compare user choice to computer choice
+# determine winner
+# prompt user to play again
+# if user quits, show tally of wins and loses
